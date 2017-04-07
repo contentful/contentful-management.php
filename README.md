8cf818782336941522de8e235b7b2e71959dda9a
Rouven Weßling <rouven@contentful.com>
2017-04-07 19:29:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+contentful-management.php
+===============
+
+PHP SDK for [Contentful's][1] Content Management API.
+
+[Contentful][1] is a content management platform for web applications, mobile apps and connected devices. It allows you to create, edit & manage content in the cloud and publish it anywhere via powerful API. Contentful offers tools for managing editorial teams and enabling cooperation between organizations.
+
+The SDK requires at least PHP 5.5.9. PHP 7 is supported.
+
+The SDK is currently in beta. The API might change at any time.
+
+Setup
+=====
+
+To add this package to your `composer.json` and install it execute the following command:
+
+```bash
+php composer.phar require contentful/contentful-management:@dev
+````
+
+Then, if not already done, include the Composer autoloader:
+
+```php
+require_once 'vendor/autoload.php';
+```
+
+License
+=======
+
+Copyright (c) 2015-2017 Contentful GmbH. Code released under the MIT license. See [LICENSE][2] for further details.
+
+ [1]: https://www.contentful.com
+ [2]: LICENSE
---
+++ src/Management/Client.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @copyright 2015-2017 Contentful GmbH
+ * @license   MIT
+ */
+
+namespace Contentful\Management;
+
+use Contentful\Client as BaseClient;
+use Contentful\Log\LoggerInterface;
+use GuzzleHttp\ClientInterface as GuzzleClientInterface;
+
+class Client extends BaseClient
+{
+    const VERSION = '0.6.0-dev';
+
+    /**
+     * Client constructor.
+     *
+     * @param string $token
+     * @param array $options
+     *
+     * @api
+     */
+    public function __construct($token, array $options = [])
+    {
+        $baseUri = 'https://api.contentful.com/';
+        $api = 'MANAGEMENT';
+
+        $options = array_replace([
+            'guzzle' => null,
+            'logger' => null,
+            'uriOverride' => null
+        ], $options);
+
+        $guzzle = $options['guzzle'];
+        $logger = $options['logger'];
+        $uriOverride = $options['uriOverride'];
+
+        if ($uriOverride !== null) {
+            $baseUri = $uriOverride;
+        }
+        $baseUri .= 'spaces/';
+
+        parent::__construct($token, $baseUri, $api, $logger, $guzzle);
+    }
+
+    /**
+     * The name of the library to be used in the User-Agent header.
+     *
+     * @return string
+     */
+    protected function getUserAgentAppName()
+    {
+        return 'ContentfulCMA/' . self::VERSION;
+    }
+}
