b3e9d1c081c1998fd98c69fc6c391123df4b1037
Rouven Weßling <rouven@contentful.com>
2017-04-08 13:14:07 +0200

    Sets up test recording in way that doesn't record the Authorization header.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,15 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use VCR\Event\BeforeRecordEvent;
+
+function cleanRequest(BeforeRecordEvent $event, $eventName)
+{
+    $request = $event->getRequest();
+    // Remove the Authorization header to prevent leaking CMA tokens
+    $request->removeHeader('Authorization');
+}
+
 /**
  * @param \VCR\Request $request
  *
@@ -15,6 +24,11 @@ function clean_headers_array(\VCR\Request $request)
         }
 
         if (strtolower($name) === 'user-agent' || strtolower($name) === 'x-contentful-user-agent') {
+            return false;
+        }
+
+        // Since we omit the Authorization header from recordings we can't mach on it
+        if (strtolower($name) === 'authorization') {
             return false;
         }
 
@@ -33,6 +47,8 @@ function clean_headers_array(\VCR\Request $request)
         return $first == $second;
     })
     ->enableRequestMatchers(['method', 'url', 'query_string', 'host', 'body', 'post_fields', 'custom_headers']);
+
+\VCR\VCR::getEventDispatcher()->addListener(\VCR\VCREvents::VCR_BEFORE_RECORD, 'cleanRequest');
 
 \VCR\VCR::turnOn();
 \VCR\VCR::turnOff();
