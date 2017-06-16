97fd26e7b61972874c946c2fdeaab763ae135229
Rouven Weßling <rouven@contentful.com>
2017-06-16 17:09:56 +0200

    Implements some CMA specific errors by mapping them to exceptions
    Errors mapped:
     - BadRequest
     - MissingKey
     - UnknownKey
     - UnsupportedMediaType
     - ValidationFailed
     - VersionMismatch

---
+++ tests/Unit/Exception/MissingKeyExceptionTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @copyright 2015-2017 Contentful GmbH
+ * @license   MIT
+ */
+
+namespace Contentful\Tests\Unit\Exception;
+
+use Contentful\Management\Exception\MissingKeyException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+
+class MissingKeyExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExceptionStructure()
+    {
+        $request = new Request('POST', 'https://api.contentful.com/spaces/34luz0flcmxt/locales');
+        $response = new Response(
+            400,
+            ['X-Contentful-Request-Id' => '761a55845f700069f3c3b4dae6ad1117'],
+            '{"requestId":"761a55845f700069f3c3b4dae6ad1117","details":{"key":"resource"},"sys":{"type":"Error","id":"MissingKey"}}',
+            1.1,
+            'Bad Request'
+        );
+
+        $guzzleException = new ClientException('This is an error', $request, $response);
+        $exception = new MissingKeyException($guzzleException);
+
+        $this->assertTrue($exception->hasResponse());
+        $this->assertSame($request, $exception->getRequest());
+        $this->assertSame($response, $exception->getResponse());
+        $this->assertEquals('761a55845f700069f3c3b4dae6ad1117', $exception->getRequestId());
+        $this->assertEquals('This is an error', $exception->getMessage());
+        $this->assertEquals('resource', $exception->getKey());
+    }
+}
---
+++ tests/Unit/Exception/VersionMismatchExceptionTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @copyright 2015-2017 Contentful GmbH
+ * @license   MIT
+ */
+
+namespace Contentful\Tests\Unit\Exception;
+
+use Contentful\Management\Exception\VersionMismatchException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+
+class VersionMismatchExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    public function testExceptionStructure()
+    {
+        $request = new Request('POST', 'https://api.contentful.com/spaces/34luz0flcmxt/locales');
+        $response = new Response(
+            409,
+            ['X-Contentful-Request-Id' => '0b22bcf71e0d624fbd878bc398746d2e'],
+            '{"sys": {"type": "Error","id": "VersionMismatch"},"requestId": "0b22bcf71e0d624fbd878bc398746d2e"}',
+            1.1,
+            'Conflict'
+        );
+
+        $guzzleException = new ClientException('This is an error', $request, $response);
+        $exception = new VersionMismatchException($guzzleException);
+
+        $this->assertTrue($exception->hasResponse());
+        $this->assertSame($request, $exception->getRequest());
+        $this->assertSame($response, $exception->getResponse());
+        $this->assertEquals('0b22bcf71e0d624fbd878bc398746d2e', $exception->getRequestId());
+        $this->assertEquals('This is an error', $exception->getMessage());
+    }
+}
