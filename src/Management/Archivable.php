d0483bcc96f209c8ff076ed4abd64c7d4e1f4f07
Davide Borsatto <davide.borsatto@gmail.com>
2017-06-26 13:48:12 +0200

    Updated documentation of interfaces

--- src/Management/Archivable.php
+++ src/Management/Archivable.php
@@ -1,11 +1,22 @@
 <?php
+
 /**
- * @copyright 2015-2017 Contentful GmbH
+ * This file is part of the contentful-management.php package.
+ *
+ * @copyright 2017 Contentful GmbH
  * @license   MIT
  */
 
 namespace Contentful\Management;
 
+/**
+ * Archivable interface.
+ *
+ * Represents an entity which can be archived.
+ *
+ * @see https://www.contentful.com/developers/docs/references/content-management-api/#/reference/assets/asset-archiving Archiving assets
+ * @see https://www.contentful.com/developers/docs/references/content-management-api/#/reference/entries/entry-archiving Archiving entries
+ */
 interface Archivable extends SpaceScopedResourceInterface
 {
 }
--- src/Management/Publishable.php
+++ src/Management/Publishable.php
@@ -1,11 +1,23 @@
 <?php
+
 /**
- * @copyright 2015-2017 Contentful GmbH
+ * This file is part of the contentful-management.php package.
+ *
+ * @copyright 2017 Contentful GmbH
  * @license   MIT
  */
 
 namespace Contentful\Management;
 
+/**
+ * Publishable interface.
+ *
+ * Represents an entity which can be published.
+ *
+ * @see https://www.contentful.com/developers/docs/references/content-management-api/#/reference/assets/asset-publishing Publishing assets
+ * @see https://www.contentful.com/developers/docs/references/content-management-api/#/reference/content-types/content-type-activation Publishing content types
+ * @see https://www.contentful.com/developers/docs/references/content-management-api/#/reference/entries/entry-publishing Publishing entries
+ */
 interface Publishable extends SpaceScopedResourceInterface
 {
 }
