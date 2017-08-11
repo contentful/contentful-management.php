6a75c39d47ba38ceb35e0b7ef2dd4d90babb137f
Davide Borsatto <davide.borsatto@gmail.com>
2017-08-11 11:58:20 +0200

    Adds support for space memberships

---
+++ src/Management/Resource/SpaceMembership.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * This file is part of the contentful-management.php package.
+ *
+ * @copyright 2015-2017 Contentful GmbH
+ * @license   MIT
+ */
+
+namespace Contentful\Management\Resource;
+
+use Contentful\Link;
+use Contentful\Management\Behavior\Creatable;
+use Contentful\Management\Behavior\Deletable;
+use Contentful\Management\Behavior\Updatable;
+
+/**
+ * SpaceMembership class.
+ *
+ * This class represents a resource with type "SpaceMembership" in Contentful.
+ *
+ * @see https://www.contentful.com/developers/docs/references/content-management-api/#/reference/space-memberships
+ */
+class SpaceMembership extends BaseResource implements Creatable, Updatable, Deletable
+{
+    /**
+     * @var bool
+     */
+    protected $admin = false;
+
+    /**
+     * @var string|null
+     */
+    protected $email;
+
+    /**
+     * @var Link[]
+     */
+    protected $roles = [];
+
+    /**
+     * @var Link|null
+     */
+    protected $user;
+
+    /**
+     * SpaceMembership constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct('SpaceMembership');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getResourceUriPart(): string
+    {
+        return 'space_memberships';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return $this->admin;
+    }
+
+    /**
+     * @param bool $admin
+     *
+     * @return $this
+     */
+    public function setAdmin(bool $admin)
+    {
+        $this->admin = $admin;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return $this
+     */
+    public function setEmail(string $email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return Link[]
+     */
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param Link $role
+     *
+     * @return $this
+     */
+    public function addRole(Link $role)
+    {
+        $this->roles[] = $role;
+
+        return $this;
+    }
+
+    /**
+     * @param Link[] $roles
+     *
+     * @return $this
+     */
+    public function setRoles(array $roles = [])
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @return Link|null
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Returns an array to be used by `json_encode` to serialize objects of this class.
+     *
+     * @return array
+     *
+     * @see http://php.net/manual/en/jsonserializable.jsonserialize.php JsonSerializable::jsonSerialize
+     */
+    public function jsonSerialize(): array
+    {
+        $spaceMembership = [
+            'sys' => $this->sys,
+            'admin' => $this->admin,
+            'roles' => $this->roles,
+        ];
+
+        if ($this->email) {
+            $spaceMembership['email'] = $this->email;
+        }
+
+        return $spaceMembership;
+    }
+}
---
+++ tests/Unit/Resource/SpaceMembershipTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file is part of the contentful-management.php package.
+ *
+ * @copyright 2015-2017 Contentful GmbH
+ * @license   MIT
+ */
+
+namespace Contentful\Tests\Unit\Resource;
+
+use Contentful\Link;
+use Contentful\Management\Resource\SpaceMembership;
+use PHPUnit\Framework\TestCase;
+
+class SpaceMembershipTest extends TestCase
+{
+    public function testGetSetData()
+    {
+        $spaceMembership = new SpaceMembership();
+
+        $spaceMembership->setEmail('php-cma-sdk-tests-eb2a4f5@contentful.com');
+        $this->assertEquals('php-cma-sdk-tests-eb2a4f5@contentful.com', $spaceMembership->getEmail());
+
+        $spaceMembership->setAdmin(true);
+        $this->assertTrue($spaceMembership->isAdmin());
+
+        $this->assertEmpty($spaceMembership->getRoles());
+
+        $role1 = new Link('6khUMmsfVslYd7tRcThTgE', 'Role');
+        $role2 = new Link('6kj2AeUS0kJbHr0F2xCjIU', 'Role');
+        $spaceMembership->addRole($role1);
+        $this->assertEquals([$role1], $spaceMembership->getRoles());
+        $spaceMembership->setRoles([$role1, $role2]);
+        $this->assertEquals([$role1, $role2], $spaceMembership->getRoles());
+
+        $this->assertNull($spaceMembership->getUser());
+    }
+
+    public function testJsonSerialize()
+    {
+        $spaceMembership = (new SpaceMembership())
+            ->setEmail('php-cma-sdk-tests-eb2a4f5@contentful.com')
+            ->setAdmin(false)
+            ->addRole(new Link('6khUMmsfVslYd7tRcThTgE', 'Role'));
+
+        $json = '{"sys":{"type":"SpaceMembership"},"email":"php-cma-sdk-tests-eb2a4f5@contentful.com","admin":false,"roles":[{"sys":{"type":"Link","linkType":"Role","id":"6khUMmsfVslYd7tRcThTgE"}}]}';
+
+        $this->assertJsonStringEqualsJsonString($json, json_encode($spaceMembership));
+    }
+}
