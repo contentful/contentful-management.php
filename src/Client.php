cb8c9a853025601f4252e0de435d8dacb3accad9
Davide Borsatto <davide.borsatto@gmail.com>
2018-03-26 17:31:58 +0200

    chore(release): Prepare development of next version

--- src/Client.php
+++ src/Client.php
@@ -32,7 +32,7 @@ class Client extends BaseClient
      *
      * @var string
      */
-    const VERSION = '1.0.0-beta1';
+    const VERSION = '1.0.0-dev';
 
     /**
      * The default URI to which all requests should be made.
