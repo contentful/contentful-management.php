b1db34de6d9c560abc0a07137a85a887986e9b03
Sebastian <sebb@sebb767.de>
2021-06-04 01:39:45 +0200

    Fixed header comparison
    This commit fixes an error in the php-vcr filter which did not match an
    empty and a null value for expect. The fix ignores expecct values for
    now.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -30,32 +30,41 @@ function clean_headers_array(Request $request)
             return false;
         }
 
-        return !\in_array(\mb_strtolower($name), [
+        $name_lower = \mb_strtolower($name);
+
+        return !\in_array($name_lower, [
             'user-agent',
             'x-contentful-user-agent',
             'authorization',
+            'expect'
         ], true);
     }, \ARRAY_FILTER_USE_BOTH);
 }
 
 // The VCR needs to be loaded before the Client is loaded for the first time or it will fail
 VCR::configure()
-    //->setMode(VCR::MODE_ONCE)
-    ->setMode('new_episodes')
+    ->setMode(VCR::MODE_ONCE)
     ->setStorage('json')
     ->setCassettePath('tests/Recordings')
     ->addRequestMatcher('custom_headers', function (Request $first, Request $second) {
-        return clean_headers_array($first) === clean_headers_array($second);
+        $a = clean_headers_array($first);
+        $b = clean_headers_array($second);
+        $result = $a === $b;
+        if ($result)
+            return $result;
+        var_dump($a);
+        var_dump($b);
+        return $result;
     })
     ->enableRequestMatchers(['method', 'url', 'query_string', 'host', 'body', 'post_fields', 'custom_headers'])
 ;
 
 // Remove the Authorization header to prevent leaking CMA tokens
 VCR::getEventDispatcher()
+    //->addListener(VCREvents::)
     ->addListener(VCREvents::VCR_BEFORE_RECORD, function (BeforeRecordEvent $event) {
         $event->getRequest()->removeHeader('Authorization');
-    })
-;
+    }) ;
 
 VCR::turnOn();
 VCR::turnOff();
